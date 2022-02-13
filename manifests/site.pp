4118a3f532ae4cf9c5439d180152e1528e2b40a1
Abatcha.Olloh <abatchaolloh@outlook.fr>
2022-02-13 16:30:00 +0000

    [site] modified compute1 motd

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,6 +1,12 @@
 # site.pp
 #  include role::webserver
-node default {
-class { 'tmux':
+#node default {
+#class { 'tmux':
+#}
+#}
+
+node compute1.uni.local {
+file { '/etc/motd':
+content => 'My Testing content',
 }
 }
